2c266e3a337ce4ea88863cbb971b0be13e097962
inuld4ta06@gmail.com <inuld4ta06@gmail.com@619437bc-bc77-fc6c-86cd-bc29384ee27b>
2014-07-15 03:40:24 +0000

    layout template lebih distabilkan

--- mawarkeu3/page/menuHeader.php
+++ mawarkeu3/page/menuHeader.php
@@ -1,8 +1,89 @@
-<a href="javascript:void(0)" class="easyui-linkbutton" data-options="plain:true" onclick="openMenu('Home', '');">Home</a>
-<a href="javascript:void(0)" class="easyui-linkbutton" data-options="plain:true" onclick="openMenu('Pembayaran Siswa', 'pembayaranSiswa/cPembayaranSiswa');">Pembayaran Siswa</a>
-<a href="javascript:void(0)" class="easyui-linkbutton" data-options="plain:true" onclick="openMenu('Laporan', 'cLaporan');">Laporan</a>
-<a href="javascript:void(0)" class="easyui-menubutton" data-options="menu:'#menuDrop'">Setting</a>
-<div id='menuDrop' style='width: 150px;'>
-    <div onclick="openMenu('Jenis Pembayaran', 'cJenisPembayaran');">Jenis Pembayaran</div>
-    <div onclick="openMenu('Default Pembayaran', 'cDefaultPembayaran');">Default Pembayaran</div>
-</div>
+<?php
+session_start();
+$username = $_SESSION['user_name'];
+
+// si user dapat role apa aja
+$q1 = <<<q
+select * from users_role where user_name='$username'
+q;
+$role = array();
+foreach (pdoMysql_queryAll($q1) as $row1){
+    array_push($role, $row1['user_role']);
+}
+$roles = "'" . implode("','", $role) . "'";
+
+// role itu tadi dapat menu apa aja
+$q2 = <<<q
+select * from m_menu_role where role_id in ($roles)
+q;
+$menu = array();
+foreach (pdoMysql_queryAll($q2) as $row2){
+    array_push($menu, $row2['menu_id']);
+}
+$menus = implode(",", $menu);
+
+// menu2 itu tadi ada di header mana aja
+$q3 = <<<q
+select * from m_menu where menu_id in (select parent_id from m_menu where menu_id in ($menus) and enabled=1) and type='MENU_HEADER'
+q;
+
+foreach (pdoMysql_queryAll($q3) as $row3){
+    $menuheaderName = $row3['name'];
+    $menuheaderId = $row3['menu_id'];
+        $html[] = <<<h
+<a href="javascript:void(0)" class="easyui-menubutton" data-options="menu:'#menu$menuheaderName'">$menuheaderName</a>
+<div id="menu$menuheaderName" style="width:200px">
+h;
+        $q4 = "select * from m_menu where menu_id in ($menus) and parent_id=$menuheaderId";
+        foreach (pdoMysql_queryAll($q4) as $row4){
+            $menuName = $row4['name'];
+            $menuPath = $row4['path'];
+            $html[] = <<<h
+<div onclick="openMenu('$menuName', '$menuPath');">$menuName</div>
+h;
+        }
+        $html[] = <<<h
+        </div>
+h;
+}
+
+if(count($html) > 0){
+    $menus_ready = implode('', $html);
+} else {
+    $menus_ready = "";
+}
+
+echo <<<h
+<table width="100%">
+    <tr>
+        <td>$menus_ready</td>
+        <td style="vertical-align: middle;text-align: right;">
+            <a href="javascript:void(0)" class="easyui-linkbutton" data-options="plain:true" iconCls="icon-no" onclick="logout();" title="Logout"><b>$username</b></a>
+        </td>
+    </tr>
+</table>
+
+h;
+
+
+function pdoMysql_queryAll($sql) {
+    $server = DB_HOST;
+    $username = DB_USER;
+    $password = DB_PASS;
+    $dbase = DB_NAME;
+    try {
+        $db = new PDO("mysql:host=$server;dbname=$dbase", $username, $password);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        $data = array();
+        $result = $db->query($sql);
+        foreach ($result as $row) {
+            array_push($data, $row);
+        }
+
+        $db = null; // close the database connection
+        return $data;
+    } catch (PDOException $e) {
+        return $e->getMessage();
+    }
+}
--- mawarkeu3/page/template.php
+++ mawarkeu3/page/template.php
@@ -7,12 +7,19 @@ $username = $_SESSION['user_name'];
     <head>
         <meta charset="UTF-8">
         <title>Aplikasi Keuangan Yayasan Pendidikan Matholi'ul Anwar Simo Sungelebak Karanggeneng Lamongan</title>
-        <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/<?= $themes ?>/easyui.css">
-        <link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/icon.css">
-        <script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery-1.8.0.min.js"></script>
-        <script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.easyui.min.js"></script>
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="icon" href="favicon.ico" type="image/x-icon" >
+        <!--<link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/<?= $themes ?>/easyui.css">-->
+        <link rel="stylesheet" type="text/css" href="include/easyui/themes/default/easyui.css">
+        <!--<link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/icon.css">-->
+        <link rel="stylesheet" type="text/css" href="include/easyui/themes/icon.css">
+        <!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>-->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+        <!--<script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.easyui.min.js"></script>-->
+        <script type="text/javascript" src="include/easyui/jquery.easyui.min.js"></script>
+        <!--<script type="text/javascript" src="http://www.jeasyui.com/easyui/datagrid-detailview.js"></script>-->
         <script type="text/javascript" src="http://www.jeasyui.com/easyui/datagrid-detailview.js"></script>
-        <script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.edatagrid.js"></script>
+        <!--<script type="text/javascript" src="http://www.jeasyui.com/easyui/jquery.edatagrid.js"></script>-->
         <?php
         /* ---------------- Definisi situs ---------------- */
         define("HEADERKETERANGANSEKOLAH01", "Sistem Keuangan");
@@ -20,61 +27,55 @@ $username = $_SESSION['user_name'];
         define("HEADERKETERANGANSEKOLAH03", "Simo Sungelebak Karanggeneng Lamongan");
         define("FOOTER_TEXT", "Copyright &copy; " . date('Y') . " by Yayasan Pendidikan Matholi'ul Anwar, Simo, Sungelebak, Karanggeneng, Lamongan");
         ?> 
+        
+        <script>
+
+            $.extend($.fn.datebox.defaults, {
+                formatter: function(date) {
+                    var y = date.getFullYear();
+                    var m = date.getMonth() + 1;
+                    var d = date.getDate();
+                    return  y + '-' + (m < 10 ? ('0' + m) : m) + '-' + (d < 10 ? ('0' + d) : d);
+                },
+                parser: function(s) {
+                    if (!s)
+                        return new Date();
+                    var ss = s.split('-');
+                    var d = parseInt(ss[2], 10);
+                    var m = parseInt(ss[1], 10);
+                    var y = parseInt(ss[0], 10);
+                    if (!isNaN(y) && !isNaN(m) && !isNaN(d)) {
+                        return new Date(y, m - 1, d);
+                    } else {
+                        return new Date();
+                    }
+                }
+            });
+
+            var index = 0;
+            function openMenu(nmMenu, url) {
+                index++;
+                if ($('#mawarkeu3_tt').tabs('exists', nmMenu)) {
+                    $('#mawarkeu3_tt').tabs('select', nmMenu);
+                } else {
+                    $('#mawarkeu3_tt').tabs('add', {
+                        title: nmMenu,
+                        href: 'ajaxroute.php?p=' + url,
+                        closable: true
+                    });
+                }
+            }
+
+            function logout() {
+                window.location.replace("?logout");
+            }
+        </script>
+        
     </head>
     <body class="easyui-layout">
+        <div data-options="region:'north', split:false, href:'ajaxroute.php?p=menuHeader'" style="height: 45px;padding:5px;border:1px solid #ddd;">
+        </div>
         <div data-options="region:'center'" >
-            <script>
-
-                $.extend($.fn.datebox.defaults, {
-                    formatter: function(date) {
-                        var y = date.getFullYear();
-                        var m = date.getMonth() + 1;
-                        var d = date.getDate();
-                        return  y + '-' + (m < 10 ? ('0' + m) : m) + '-' + (d < 10 ? ('0' + d) : d);
-                    },
-                    parser: function(s) {
-                        if (!s)
-                            return new Date();
-                        var ss = s.split('-');
-                        var d = parseInt(ss[2], 10);
-                        var m = parseInt(ss[1], 10);
-                        var y = parseInt(ss[0], 10);
-                        if (!isNaN(y) && !isNaN(m) && !isNaN(d)) {
-                            return new Date(y, m - 1, d);
-                        } else {
-                            return new Date();
-                        }
-                    }
-                });
-
-                var index = 0;
-                function openMenu(nmMenu, url) {
-                    index++;
-                    if ($('#mawarkeu3_tt').tabs('exists', nmMenu)) {
-                        $('#mawarkeu3_tt').tabs('select', nmMenu);
-                    } else {
-                        $('#mawarkeu3_tt').tabs('add', {
-                            title: nmMenu,
-                            href: 'ajaxroute.php?p=' + url,
-                            closable: true
-                        });
-                    }
-                }
-
-                function logout() {
-                    window.location.replace("?logout");
-                }
-            </script>
-            <div style="padding:5px;border:1px solid #ddd">
-                <table width="100%">
-                    <tr>
-                        <td><?php include 'menuHeader.php'; ?></td>
-                        <td style="vertical-align: middle;text-align: right;">
-                            <a href="javascript:void(0)" class="easyui-linkbutton" data-options="plain:true" iconCls="icon-no" onclick="logout();" title="Logout"><b><?= $username ?></b></a>
-                        </td>
-                    </tr>
-                </table>
-            </div>
             <div id='mawarkeu3_tt' class="easyui-tabs" data-options="fit: true">
                 <div title="Home" style="padding: 10px;background: url('css/login/images/bg.jpg');color:white;">
                     <?= HEADERKETERANGANSEKOLAH01 ?><br>
